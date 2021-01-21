8012f21a3c0a37d763b160e776439714afbc4d8d
Balu Sai <balusai@itversity.in>
2021-01-21 13:02:29 +0530

    Added pg connections

--- pgCreate/createUser.py
+++ pgCreate/createUser.py
@@ -6,21 +6,31 @@
 # * grant access to given list of Databases on username
 # * Add username to a pg_users_active.csv
 import sys
-import 
+import psycopg2
 
+host='localhost'
+port='5433'
+pguser='admin'
+pgpassword='hNg9X7CYcDLv'
+conn = psycopg2.connect(
+    host=host,
+    port=port,
+    database='postgres',
+    user=pguser,
+    password=pgpassword)
 
-# createRole(user,password)
-#
-# return 1
-#
-# CreateDB(user,password)
-
+def createRole(user,password):
+    print(conn,user,password)
+    conn.close()
+    print("Connection closed")
+    conn
 
 def createUsers():
-
     username= sys.argv[1]
     paswd= sys.argv[2]
     print(username,paswd)
+    createRole(username,paswd)
+
 createUsers()
-# createRole(username,paswd)
+
 # CreateDB(username,paswd)
