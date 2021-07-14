affa53dfeaa29bbe70c6198107fd62f2272ef0b6
Balu Sai D <balusai@itversity.in>
2021-07-14 15:08:11 +0530

    Added createdb script with revoke

--- pgCreate/createUser.py
+++ pgCreate/createUser.py
@@ -1,13 +1,7 @@
-# * Create a user/role with username and password on postgres with login
-    # * Username and password are supplied as stdin(args)
-# * Create Databases for user with prefix username
-#     username_hr_db
-#     username_retail_db
-# * grant access to given list of Databases on username
-# * Add username to a pg_users_active.csv
 import sys
 import psycopg2
 import os
+import logging
 
 database_list=['hr_db','sms_db','retail_db']
 database_user_list=['hr_user','sms_user','retail_user']
@@ -26,17 +20,24 @@ cur = conn.cursor()
 
 def createRole(user,password):
     for i in database_user_list:
-      cur.execute(f"""
-      CREATE ROLE {user}_{i} with NOCREATEDB LOGIN ENCRYPTED PASSWORD '{password}';""")
+      cur.execute(f"""CREATE ROLE {user}_{i} with NOCREATEDB LOGIN ENCRYPTED PASSWORD '{password}';""")
     return ;
 
 def CreateDB(user,paswd):
     for i in database_list:
         cur.execute(f"""CREATE DATABASE {user}_{i};""")
 
+def accessPermissions(user):
+    for i in database_list:
+        cur.execute(f"""revoke ALL on DATABASE {user}_{i} FROM PUBLIC;""")
+    for i in range(len(database_user_list)):
+        cur.execute(f"""Grant ALL on DATABASE {user}_{database_list[i]} TO {user}_{database_user_list[i]};""")
+
+
 def createUsers():
     username= sys.argv[1]
     paswd= sys.argv[2]
+    userEmail=sys.argv[3]
     createRole(username,paswd)
     CreateDB(username,paswd)
     conn.close()
